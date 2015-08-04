2277dd699bbb5f5aadd7a771ea45963f39e47c1a
Katy Justiss <katy.justiss@gmail.com>
2015-08-04 10:39:33 -0500

    added online logging function

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+var fs = require('fs');
 //npm requires
 var express = require('express');
+var morgan = require('morgan');
+var loggly = require('loggly');
 
 //required routes
 var routes = require('./routes/index')
@@ -17,13 +20,35 @@ app.set('view engine', 'ejs');
 app.locals.title = 'aweso.me';
 
 //middlewares
-//logging
 app.use(require('less-middleware')('public'));
 
-app.use(function(req, res, next) {
-  console.log('Request at ' + new Date().toISOString());
-  next();
+//logging
+// app.use(function(req, res, next) {
+//   console.log('Request at ' + new Date().toISOString());
+//   next();
+// });
+//Write logs to file.
+var logStream = fs.createWriteStream('access.log', {flags: 'a'});
+app.use(morgan('combined', {stream: logStream}));
+app.use(morgan('dev'));
+
+var client = loggly.createClient({
+    token: "41698293-05d9-4603-a8e8-0b82920849df",
+    subdomain: "katyjustiss",
+    tags: ["NodeJS"],
+    json:true
 });
+
+//can just trim this down to what you want
+app.use(function(req, res, next) {
+  client.log({req: req.ip,
+              date: new Date().toISOString(),
+              url: req.url,
+              status: res.statusCode,
+              method: req.method
+            });
+  next();
+})
 
 app.use(express.static('public'));
 
