4307fd8f4ade7569b1816b0d07a813a300b8029d
Katy Justiss <katy.justiss@gmail.com>
2015-08-05 14:05:00 -0500

    added more image capability

--- routes/imgur.js
+++ routes/imgur.js
@@ -1,13 +1,39 @@
+var fs = require('fs');
+
+var imgur = require('imgur')
 var router = require('express').Router();
-var upload  = require('multer')({dest: 'uploads/'})
+var multer = require('multer')
+
+var upload  = multer({
+  dest: 'uploads/',
+  limits: {
+    fileSize: 200 * 1000 * 1000 //megabyte calculation.
+  },
+  fileFilter: function(req, file, cb) {
+    cb(null, file.mimetype.slice(0, 6) === 'image/'); //return true or false
+  }
+});
 
 router.get('/', function(req, res) {
   res.render('templates/imgur');
 });
 
-router.post('/upload', upload.single('file'), function(req, res) {
-  console.log(req.file);
-  res.redirect('/imgur');
+router.post('/upload', upload.single('image'), function(req, res) {
+  console.log(req.file)
+  if(req.file) {
+      imgur
+        .uploadFile(req.file.path)
+        .then(function(json) {
+          fs.unlink(req.file.path, function() {
+            res.redirect(json.data.link);
+          })
+        })
+        .catch(function(err) {
+          res.sent(err);
+        })
+  } else {
+    res.status(415).send('Must upload an image!');
+  }
 });
 
 module.exports = router;
