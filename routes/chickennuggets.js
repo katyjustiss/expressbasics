4b9dc03061aa108b2fc748c0eb5c8f962a432a8c
Katy Justiss <katy.justiss@gmail.com>
2015-08-07 15:11:41 -0500

    added mongo to pizza and completed orders to chicken

--- routes/chickennuggets.js
+++ routes/chickennuggets.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var moment = require('moment');
+var ObjectID = require('mongodb').ObjectID;
 
 //var collection = global.db.collection('chickenNuggets')
 
@@ -10,10 +11,12 @@ router.get('/', function(req, res) {
   collection.find().toArray(function(err, orders) {
     formattedOrders = orders.map(function(order) { //modifying the data obj
       return {
+        _id: order._id,
         name: order.name,
-        flavor: order.style,
+        flavor: order.sauce,
         qty: order.qty,
-        createdAt: moment(order._id.getTimestamp()).fromNow()
+        createdAt: moment(order._id.getTimestamp()).fromNow(),
+        complete: order.complete
       };
     });
     res.render('templates/chicken-index', {orders: formattedOrders});
@@ -30,9 +33,23 @@ router.post('/order', function(req, res) {
   var collection = global.db.collection('chickenNuggets');
 
   collection.save(req.body, function() {
-    res.redirect('/chickennuggets/order');
+    res.redirect('/chickennuggets');
+  });
+
+});
+
+//on click set property of complete
+router.post('/order/:id/complete', function(req, res) {
+  var collection = global.db.collection('chickenNuggets');
+
+  collection.update(
+    {_id: ObjectID(req.params.id)},
+    {$set: {complete: true}},
+    function() {
+      res.redirect('/chickennuggets');
   })
 
 });
+
 
 module.exports = router;
--- routes/pizza.js
+++ routes/pizza.js
@@ -1,20 +1,57 @@
 var express = require('express');
 var router = express.Router();
+var moment = require('moment');
+var ObjectID = require('mongodb').ObjectID;
 
 
 router.get('/', function(req, res) {
-  // var obj = req.params;
-  var obj = {
-    qty: req.query.qty || 1,
-    topping: req.query.topping || 'cheese'
-  };
-  res.render('templates/pizza', obj);
+  var collection = global.db.collection('pizza');
+  // var obj = {
+  //   qty: req.query.qty || 1,
+  //   topping: req.query.topping || 'cheese'
+  // };
+  collection.find().toArray(function(err, orders) {
+    formattedOrders = orders.map(function(order) {
+      return {
+        _id: order._id,
+        name: order.name,
+        qty: order.qty,
+        topping: order.topping,
+        createdAt: moment(order._id.getTimestamp()).fromNow(),
+        complete: order.complete
+      };
+    });
+    res.render('templates/pizza-orders', {orders: formattedOrders});
+  });
+
 });
 
-// //old version with manipulation of url
-// router.get('/:topping/:qty', function(req, res) {
-//   var obj = req.params;
-//   res.render('templates/pizza', obj);
-// });
+router.get('/order', function(req, res) {
+  res.render('templates/pizza-new');
+});
+
+
+router.post('/order', function(req, res) {
+  var collection = global.db.collection('pizza');
+
+  collection.save(req.body, function() {
+    res.redirect('/pizza');
+  });
+
+});
+
+//on click set property of complete
+router.post('/order/:id/complete', function(req, res) {
+  var collection = global.db.collection('pizza');
+
+  collection.update(
+    {_id: ObjectID(req.params.id)},
+    {$set: {complete: true}},
+    function() {
+      res.redirect('/pizza');
+  })
+
+});
+
 
 module.exports = router;
