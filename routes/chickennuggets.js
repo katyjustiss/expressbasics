7c3b57de4faaaab81ee9e3b18a5b15856d8bf483
Katy Justiss <katy.justiss@gmail.com>
2015-08-07 11:11:21 -0500

    added chicken-index and new

--- routes/chickennuggets.js
+++ routes/chickennuggets.js
@@ -1,13 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
+//var collection = global.db.collection('chickenNuggets')
+
 router.get('/', function(req, res) {
-  res.render('templates/chickennuggets');
+  var collection = global.db.collection('chickenNuggets');
+
+  collection.find().toArray(function(err, orders) {
+    formattedOrders = orders.map(function(order) { //modifying the data obj
+      return {
+        name: order.name,
+        flavor: order.style,
+        qty: order.qty,
+        createdAt: order._id.getTimestamp()
+      };
+    });
+    res.render('templates/chicken-index', {orders: formattedOrders});
+  });
+
 });
 
+router.get('/order', function(req, res) {
+  res.render('templates/chicken-new');
+});
+
+
 router.post('/order', function(req, res) {
-  var collection = global.db.collection('chickenNuggets')
-  console.log(req.body);
+  var collection = global.db.collection('chickenNuggets');
 
   collection.save(req.body, function() {
     res.redirect('/');
